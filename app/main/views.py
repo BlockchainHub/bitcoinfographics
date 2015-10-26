595aebce979fdacf390c87c74746467bb0cab140
Marco Agner <marcoagner.dev@gmail.com>
2015-10-26 21:01:26 -0200

    rolls back i18n logic

--- app/main/views.py
+++ app/main/views.py
@@ -1,26 +1,17 @@
 from . import main
-from flask import render_template, abort, make_response, request, redirect
+from flask import render_template
 from .. import db
 from ..models import Infographic
 
 
-def check_lang():
-  if 'lang' in request.cookies:
-    return request.cookies['lang']
-  else:
-    return 'en'
-
-
 @main.route('/')
 def index():
-    lang = check_lang()
     infographics = Infographic.query.order_by('timestamp')
-    return render_template('index.html', infographics=infographics, lang=lang)
+    return render_template('index.html', infographics=infographics)
 
 
 @main.route('/infographic/<string:infographic_slug>')
 def infographic(infographic_slug):
-    lang = check_lang()
     infographics = Infographic.query.all()
     current_infographic = Infographic.query.filter_by(slug=infographic_slug).first_or_404()
     prev_infographic = Infographic.query.filter_by(id=current_infographic.id+1).first() or infographics[0]
@@ -28,8 +19,7 @@ def infographic(infographic_slug):
     return render_template('infographic.html',
                                infographic=current_infographic,
                                prev_slug=prev_infographic.slug,
-                               next_slug=next_infographic.slug,
-                               lang=lang)
+                               next_slug=next_infographic.slug)
 
 
 @main.route('/donate/')
@@ -40,12 +30,3 @@ def donate():
 @main.route('/about/')
 def about():
     return render_template('about.html')
-
-
-@main.route('/translate/')
-def translate():
-    referrer = request.referrer or '/'
-    response = make_response(redirect(referrer))
-    if request.args.get('lang') in ('en', 'es', 'pt'):
-      response.set_cookie('lang', request.args.get('lang'))
-    return response
